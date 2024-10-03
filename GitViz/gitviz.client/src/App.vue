8cd4fef861444334d2faa8bb1a5d7af516a132c6
sLill <samuelturnerlill@gmail.com>
2024-10-03 16:47:21 -0500

    D3.js added

--- GitViz/gitviz.client/src/App.vue
+++ GitViz/gitviz.client/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
     import { ref, computed, onMounted } from 'vue';
+    import D3Plot from '/src/components/D3Plot.vue';
 
+    const plotComponent = ref(null);
 
     // Methods
+    const updateChartData = () => {
+        const newData = [8, 3, 7, 2, 5, 11];
+        plotComponent.value.updateData(newData);
+    };
     onMounted(async () => {
 
     });
@@ -10,7 +16,8 @@
 
 <template>
     <div class="wrapper">
-       
+       <D3Plot ref="plotComponent" />
+        <button @click="updateChartData">Update Chart</button>
     </div>
 </template>
 
