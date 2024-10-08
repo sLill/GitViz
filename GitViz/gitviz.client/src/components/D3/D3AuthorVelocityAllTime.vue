b0ff40ea437f1f5c96c1b022abb13432caede916
sLill <samuelturnerlill@gmail.com>
2024-10-08 08:57:17 -0500

    Changed color palette

--- GitViz/gitviz.client/src/components/D3/D3AuthorVelocityAllTime.vue
+++ GitViz/gitviz.client/src/components/D3/D3AuthorVelocityAllTime.vue
@@ -22,20 +22,41 @@
     const containerWidth = chartContainer.value.clientWidth;
     const containerHeight = chartContainer.value.clientHeight;
   
-    const margin = { top: 20, right: 20, bottom: 120, left: 40 };
+    const margin = { top: 40, right: 20, bottom: 120, left: 60 };
     const width = containerWidth - margin.left - margin.right;
     const height = containerHeight - margin.top - margin.bottom;
   
     // Clear previous SVG
     d3.select(chartContainer.value).selectAll('*').remove();
   
-    // Create SVG
+    // Create SVG with a subtle gradient background
     svg = d3.select(chartContainer.value)
       .append('svg')
       .attr('width', containerWidth)
       .attr('height', containerHeight)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
+  
+    // Add gradient background
+    const defs = svg.append('defs');
+    const gradient = defs.append('linearGradient')
+      .attr('id', 'bg-gradient')
+      .attr('x1', '0%')
+      .attr('y1', '0%')
+      .attr('x2', '0%')
+      .attr('y2', '100%');
+    gradient.append('stop')
+      .attr('offset', '0%')
+      .attr('style', 'stop-color:#f8f9fa;stop-opacity:1');
+    gradient.append('stop')
+      .attr('offset', '100%')
+      .attr('style', 'stop-color:#e9ecef;stop-opacity:1');
+  
+    svg.append('rect')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('transform', `translate(${-margin.left},${-margin.top})`)
+      .style('fill', 'transparent');
   
     // Process data
     const processedData = Object.entries(props.data).flatMap(([name, items]) =>
@@ -60,7 +81,7 @@
   
     const color = d3.scaleOrdinal()
       .domain(Object.keys(props.data[Object.keys(props.data)[0]]))
-      .range(['green', 'red']);
+      .range(['#35a721', '#a72121']);
   
     // Draw bars
     svg.append('g')
@@ -75,7 +96,22 @@
         .attr('y', d => y(d.value))
         .attr('width', x1.bandwidth())
         .attr('height', d => height - y(d.value))
-        .attr('fill', d => color(d.key));
+        .attr('fill', d => color(d.key))
+        .attr('rx', 4)
+        .attr('ry', 4)
+      .on('mouseover', function(event, d) {
+        d3.select(this).attr('opacity', 0.8);
+        svg.append('text')
+          .attr('class', 'value-label')
+          .attr('x', x0(d.name) + x1(d.key) + x1.bandwidth() / 2)
+          .attr('y', y(d.value) - 5)
+          .attr('text-anchor', 'middle')
+          .text(d.value);
+      })
+      .on('mouseout', function() {
+        d3.select(this).attr('opacity', 1);
+        svg.selectAll('.value-label').remove();
+      });
   
     // Add x-axis with vertical labels
     svg.append('g')
@@ -85,19 +121,25 @@
         .style('text-anchor', 'end')
         .attr('dx', '-.8em')
         .attr('dy', '.15em')
-        .attr('transform', 'rotate(-90)');
+        .attr('transform', 'rotate(-65)')
+        .style('font-size', '12px')
+        .style('fill', '#495057');
   
     // Add y-axis
     svg.append('g')
       .call(d3.axisLeft(y).ticks(null, 's'))
+      .style('font-size', '12px')
+      .call(g => g.select('.domain').remove())
+      .call(g => g.selectAll('.tick line').clone()
+          .attr('x2', width)
+          .attr('stroke-opacity', 0.1))
       .append('text')
-        .attr('x', 2)
-        .attr('y', y(y.ticks().pop()) + 0.5)
-        .attr('dy', '0.32em')
-        .attr('fill', '#000')
+        .attr('x', -margin.left + 10)
+        .attr('y', -20)
+        .attr('fill', '#495057')
         .attr('font-weight', 'bold')
         .attr('text-anchor', 'start')
-        .text('Value');
+        .text('Lines of Code');
   
     // Add legend
     const legend = svg.append('g')
@@ -113,7 +155,9 @@
       .attr('x', width - 19)
       .attr('width', 19)
       .attr('height', 19)
-      .attr('fill', color);
+      .attr('fill', color)
+      .attr('rx', 4)
+      .attr('ry', 4);
   
     legend.append('text')
       .attr('x', width - 24)
@@ -143,5 +187,6 @@
   .chart-container {
     width: 100%;
     height: 100%;
+    font-family: Arial, sans-serif;
   }
   </style>
